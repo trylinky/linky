96d0f9de6a2498c20f9bb2c190e2c112a0a68708
Alex Pate <git@alexjpate.com>
2025-11-28 15:44:27 +0100

    Require icons

--- packages/common/src/billing/pricing-table.tsx
+++ packages/common/src/billing/pricing-table.tsx
@@ -6,6 +6,7 @@ import { auth } from '../auth/auth';
 import { LoginWidget } from '../auth/login-widget';
 import {
   ArrowTrendingDownIcon,
+  ArrowTrendingUpIcon,
   ArrowUpIcon,
   CheckBadgeIcon,
   CubeIcon,
@@ -15,10 +16,7 @@ import {
   UsersIcon,
   LockClosedIcon,
   GlobeAltIcon,
-  PuzzlePieceIcon,
-  PaintBrushIcon,
 } from '@heroicons/react/24/outline';
-import { ArrowTrendingUpIcon } from '@heroicons/react/24/outline';
 import {
   Alert,
   AlertDescription,
