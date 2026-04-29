5e12af0e320e51307e0909fccb25086a5ab42b83
Alex Pate <alex.pate@stackblitz.com>
2026-04-29 12:02:16 +0100

    export models

--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -1,2 +1,4 @@
 export { PrismaClient } from './src/generated/client.ts';
 export type { Prisma } from './src/generated/client.ts';
+export * from './src/generated/models.ts';
+export * from './src/generated/enums.ts';
